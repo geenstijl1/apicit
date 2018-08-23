a9b851720ebdcdd5c90d647c6daf0b63fea4ecdd
JamesACM <jaimecanalesmarin@gmail.com>
2018-08-23 15:00:04 -0300

    default user admin

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create(username: "admin", email: "admin@domain.com", password: "admin123",password_confirmation: "admin123", role: 0)
